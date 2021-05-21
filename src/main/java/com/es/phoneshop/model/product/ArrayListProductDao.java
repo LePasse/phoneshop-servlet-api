68608ff008f0335cbd5492bc0ba4e0bc31326e71
LePasse <43090385+LePasse@users.noreply.github.com>
2021-05-21 17:05:09 +0300

    1.5 Add Thread safety

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -7,51 +7,81 @@ import java.util.ArrayList;
 import java.util.Currency;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.stream.Collectors;
 
 public class ArrayListProductDao implements ProductDao {
 
     private List<Product> products;
     private long maxId;
+    private static ReentrantReadWriteLock locker;
 
     public ArrayListProductDao(){
         this.products = new ArrayList<>();
         getSampleProducts();
+        locker = new ReentrantReadWriteLock();
         maxId = 0;
     }
 
     @Override
     public Optional<Product> getProduct(Long id) {
-        return products.stream()
-                .filter(p -> id.equals(p.getId()))
-                .findAny();
+        locker.writeLock().lock();
+        try {
+            return products.stream()
+                    .filter(product -> id.equals(product.getId()))
+                    .findAny();
+        } finally {
+            locker.writeLock().unlock();
+        }
     }
 
     @Override
     public List<Product> findProducts() {
-        return products;
+        locker.writeLock().lock();
+        try {
+            return products.stream()
+                    .filter(product -> product.getPrice() != null)
+                    .filter(product -> product.getStock() > 0)
+                    .collect(Collectors.toList());
+        } finally {
+            locker.writeLock().unlock();
+        }
     }
 
     @Override
     public void save(Product product) {
-        if (product.getId() == null) {
-            product.setId(maxId++);
-        } else {//check for id duplication
-            Optional<Product> productCopy = getProduct(product.getId());
-            if (productCopy.isPresent()) {
-                products.remove(productCopy.get());
-            }
-            else {
+        locker.readLock().lock();
+        try {
+            if (product.getId() == null) { //id net == new object
                 product.setId(maxId++);
+            } else {//check for id duplication
+                Optional<Product> productCopy = getProduct(product.getId());
+                if (productCopy.isPresent()) { // id est == check v spiske li
+                    products.remove(productCopy.get());
+                }
+                else { // id est i ne v spiske
+                    product.setId(maxId++);
+                }
             }
+            products.add(product);
+        } finally {
+            locker.readLock().unlock();
         }
-        products.add(product);
     }
 
 
     @Override
     public void delete(Long id) {
-        Optional<Product> product = getProduct(id);
-        product.ifPresent(value -> products.remove(value));
+        locker.readLock().lock();
+        locker.writeLock().lock();
+        try {
+            Optional<Product> product = getProduct(id);
+            product.ifPresent(value -> products.remove(value));
+        } finally {
+            locker.readLock().unlock();
+            locker.writeLock().unlock();
+        }
+
     }
 
     public void getSampleProducts(){
