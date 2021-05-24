19247420e88ae76c7ff3939377f425099c078f3d
LePasse <43090385+LePasse@users.noreply.github.com>
2021-05-24 11:28:37 +0300

    Lesson 1
    * 1.5 Implement ProductDao
    
    * 1.5 Fix ProductDao "save" method
    
    * 1.6 Use ProductDao on PLP
    
    * 1.5 Add Thread safety
    
    * 1.5 Implement ArrayListProductDaoTest
    
    * 1.5 Update ArrayListProductDaoTest
    
    * 1.5 Change ProductDao "save" method algorithm
    
    * 1.6 Implement ProductListPageServletTest
    
    * 1.5 Change ArrayListProductDaoTest method names
    
    * Delete unnecessary comments
    
    * Make code formatting

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,10 +3,14 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.List;
+import java.util.Optional;
 
-public class ArrayListProductDaoTest
-{
+import static org.junit.Assert.*;
+
+public class ArrayListProductDaoTest {
     private ProductDao productDao;
 
     @Before
@@ -16,6 +20,63 @@ public class ArrayListProductDaoTest
 
     @Test
     public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+        assertFalse(productDao.findProducts().isEmpty());
     }
+
+    @Test
+    public void testGetProduct() {
+        Optional<Product> product = productDao.getProduct(1L);
+        assertTrue(product.isPresent());
+    }
+
+    @Test
+    public void testDeleteProduct() {
+        Optional<Product> product = productDao.getProduct(2L);
+        assertTrue(product.isPresent());
+        productDao.delete(2L);
+        product = productDao.getProduct(2L);
+        assertFalse(product.isPresent());
+    }
+
+    @Test
+    public void testSaveProduct() {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product("save test", "Samsung Galaxy SS", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+        assertTrue(productDao.getProduct(product.getId()).isPresent());
+    }
+
+    @Test
+    public void testSaveExistingProduct() {
+        Currency usd = Currency.getInstance("USD");
+        Product product = new Product(22L, "save test", "Samsung Galaxy SS", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        productDao.save(product);
+        product.setCode("new test");
+        productDao.save(product);
+        Optional<Product> copy = productDao.getProduct((productDao.getMaxId() - 1));
+        assertEquals(copy.get().getCode(), "new test");
+    }
+
+    @Test
+    public void testFindPriceNullProduct() {
+        Currency usd = Currency.getInstance("USD");
+        List<Product> products = productDao.findProducts();
+        int sizeBefore = products.size();
+        productDao.save(new Product("save test", "Samsung Galaxy SS", null, usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg"));
+        products = productDao.findProducts();
+        int sizeAfter = products.size();
+        assertEquals(sizeBefore, sizeAfter);
+    }
+
+    @Test
+    public void testFindZeroStockProduct() {
+        Currency usd = Currency.getInstance("USD");
+        List<Product> products = productDao.findProducts();
+        int sizeBefore = products.size();
+        productDao.save(new Product("save test", "Samsung Galaxy SS", new BigDecimal(100), usd, 0, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg"));
+        products = productDao.findProducts();
+        int sizeAfter = products.size();
+        assertEquals(sizeBefore, sizeAfter);
+    }
+
 }
